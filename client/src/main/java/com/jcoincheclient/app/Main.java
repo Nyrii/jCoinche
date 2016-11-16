2161ece48bab3e856e868ea2fb62af91f8a39378
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-11-16 16:37:23 +0100

    Client initialization

---
+++ client/src/main/java/com/jcoincheclient/app/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by noboud_n on 16/11/2016.
+ */
+
+package com.jcoincheclient.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+    }
+}
